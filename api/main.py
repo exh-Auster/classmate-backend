30fc4828dc33cea21bf7f381039b4091cfe36288
Felipe Ribeiro <117246298+exh-Auster@users.noreply.github.com>
2024-10-26 19:57:09 -0300

    Add initial db entities

--- api/main.py
+++ api/main.py
@@ -12,6 +12,32 @@ db.bind(provider='postgres',
         host=os.environ["POSTGRES_HOST"],
         database=os.environ["POSTGRES_DATABASE"])
 
+class User(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    name = Required(str)
+    email = Required(str, unique=True)
+    password_hash = Optional(str) # TODO
+    bio = Optional(str)
+    registered_at = Optional(str) # TODO
+    groups = Set('Group') # TODO
+    posts = Set('Post') # TODO
+
+class Group(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    created_by = Required(User)
+    name = Required(str, unique=True)
+    description = Required(str)
+    posts = Set('Post') # TODO
+    
+class Post(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    author = Required(User)
+    group = Required(Group)
+    timestamp = Optional(str) # TODO
+    text_content = Required(str)
+    external_content_url = Optional(str) # TODO
+
+db.generate_mapping(create_tables=True)
 
 app = FastAPI()
 
