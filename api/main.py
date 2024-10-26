96487271e4860c9f63a1ce5412f3a16cc2e85c0a
Felipe Ribeiro <117246298+exh-Auster@users.noreply.github.com>
2024-10-26 19:56:50 -0300

    Add Pony ORM code for database connection

--- api/main.py
+++ api/main.py
@@ -1,4 +1,17 @@
+import os
+
+from datetime import date
 from fastapi import FastAPI
+from pony.orm import *
+
+db = Database()
+
+db.bind(provider='postgres',
+        user=os.environ["POSTGRES_USER"],
+        password=os.environ["POSTGRES_PASSWORD"],
+        host=os.environ["POSTGRES_HOST"],
+        database=os.environ["POSTGRES_DATABASE"])
+
 
 app = FastAPI()
 
