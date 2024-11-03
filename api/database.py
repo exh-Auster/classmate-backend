9cd1458304928a8b702fd7a0df00f0e828b67423
Felipe Ribeiro <117246298+exh-Auster@users.noreply.github.com>
2024-11-03 12:31:29 -0300

    Attempt fix for database connection

--- api/database.py
+++ api/database.py
@@ -4,18 +4,20 @@ from sqlalchemy.engine import URL
 
 from .models import *
 
-# url_object = URL.create(
-#     "postgresql",
-#     username=os.environ["POSTGRES_USER"],
-#     password=os.environ["POSTGRES_PASSWORD"],
-#     host=os.environ["POSTGRES_HOST"],
-#     database=os.environ["POSTGRES_DATABASE"],
-# )
+url_object = URL.create(
+    "postgresql",
+    username=os.environ["POSTGRES_USER"],
+    password=os.environ["POSTGRES_PASSWORD"],
+    host=os.environ["POSTGRES_HOST"],
+    database=os.environ["POSTGRES_DATABASE"],
+)
 
 # sqlite_file_name = "database.db"  
 # sqlite_url = f"sqlite:///{sqlite_file_name}"
 
-sqlite_url = os.environ["POSTGRES_URL"]
+# sqlite_url = os.environ["POSTGRES_URL"]
+
+sqlite_url = url_object
 
 engine = create_engine(sqlite_url, echo=True)
 
