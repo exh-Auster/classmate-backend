f9e46340537c9ddf6669d92e9b48f3b45a04f22b
Felipe Ribeiro <117246298+exh-Auster@users.noreply.github.com>
2024-11-03 12:18:06 -0300

    Attempt connection to external DB

--- api/database.py
+++ api/database.py
@@ -1,8 +1,23 @@
 from sqlmodel import *
 
+from sqlalchemy.engine import URL
+
 from .models import *
 
-engine = create_engine(sqlite_url, echo=True)  
+# url_object = URL.create(
+#     "postgresql",
+#     username=os.environ["POSTGRES_USER"],
+#     password=os.environ["POSTGRES_PASSWORD"],
+#     host=os.environ["POSTGRES_HOST"],
+#     database=os.environ["POSTGRES_DATABASE"],
+# )
+
+# sqlite_file_name = "database.db"  
+# sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+sqlite_url = os.environ["POSTGRES_URL"]
+
+engine = create_engine(sqlite_url, echo=True)
 
 def create_db_and_tables():  
-    SQLModel.metadata.create_all(engine) 
+    SQLModel.metadata.create_all(engine)
